e4a7dbe255b7badd25e4f083a3d966cefddc5008
shershin <michael.shershin@gmail.com>
2015-11-03 16:09:10 -0500

    started working on dbconnect

---
+++ connect.php
@@ -0,0 +1,6 @@
+<?php # CONNECT TO MySQL DATABASE.
+
+# Connect  to databse with dbname postgres user postgres and password password1
+$dbc = pg_connect ( "host=10.10.7.190 dbname=postgres user=postgres password=password1" );
+
+?>
---
+++ test.php
@@ -0,0 +1,91 @@
+<!DOCTYPE HTML>
+<html>
+
+
+
+<head>
+<title> Finder's Page </title>
+
+
+
+</head>
+
+<body>
+<?php
+$pageName = "Found Page";
+#include 'includes/pageformat.php';
+?>
+<div id="bottom">
+<?php
+require 'connect_db.php'; # gets db connection
+#require( 'includes/helpers.php' ) ; #gets helpers function
+?>
+<div id="tholder">
+<h2 style="background-color:grey; color:white; border-radius: 30px;"> Is what you found here? </h2>
+<?php
+# Show the records
+$query = 'SELECT * FROM users' ;
+
+# Execute the query
+$results = pg_query( $dbc , $query ) ;
+
+# Show results
+if( $results )
+{
+  # But...wait until we know the query succeeded before
+  # starting the table.
+  echo '<TABLE align="center">';
+  echo '<TR>';
+  echo '<TH>id</TH>';
+  echo '<TH>first name</TH>';
+  echo '<TH>Last name</TH>';
+  echo '<TH>email</TH>';
+  echo '<TH>password</TH>';
+  echo '</TR>';
+
+  # For each row result, generate a table row
+  while ( $row = pg_fetch_array( $results , PGSQL_BOTH ) )
+  {
+    echo '<TR>' ;
+    echo '<TD>' . $row['user_id'] . '</TD>' ;
+    echo '<TD>' . $row['first_name'] . '</TD>' ;
+	  echo '<TD>' . $row['last_name'] . '</TD>' ;
+	  echo '<TD>' . $row['email'] . '</TD>' ;
+	  echo '<TD>' . $row['password'] . '</TD>' ;
+    echo '</TR>' ;
+  }
+
+  # End the table
+  echo '</TABLE>';
+}
+pg_close();
+?>
+
+
+<a href="findform.php"> <h2 style="background-color: grey; color: white; border-radius: 30px;"> Didn't Find An OWNER? <br> Click HERE! </h2> </a>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
