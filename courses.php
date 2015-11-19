2d59ca49b327d87cac983abee19c0a3b5587a48a
cjb2k6 <cjb2k6@gmail.com>
2015-11-19 15:44:30 -0500

    PDF Test from Monday

--- courses.php
+++ courses.php
@@ -125,27 +125,21 @@
 
 </form>
 <script>populateList(document.getElementById('coursesOutputBox'), outputList);
-/*
+
 var doc = new jsPDF();
 
-// I know the proper spelling is colour ;)
 doc.setTextColor(100);
-doc.text(20, 20, 'This is gray.');
-
-doc.setTextColor(150);
-doc.text(20, 30, 'This is light gray.');
-
+doc.setFont("helvetica");
+doc.setFontType("bold");
+doc.setFontSize(36);
 doc.setTextColor(255, 0, 0);
-doc.text(20, 40, 'This is red.');
-
-doc.setTextColor(0, 255, 0);
-doc.text(20, 50, 'This is green.');
-
-doc.setTextColor(0, 0, 255);
-doc.text(20, 60, 'This is blue.');
+doc.text(65, 30, 'Marist College');
+doc.setFontSize(28);
+doc.setTextColor(0, 0, 0);
+doc.text(38, 40, 'Unofficial Transfer Evaluation');
 
 doc.save('Test.pdf');
-*/
+
 </script>
 </div>
 
