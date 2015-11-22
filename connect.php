04f400cfeb5f7ae58d0454f72b2ac61a5a4801ab
shershin <michael.shershin@gmail.com>
2015-11-21 22:14:10 -0500

    forgot to commit a few things

--- connect.php
+++ connect.php
@@ -1,6 +1,7 @@
 <?php # CONNECT TO MySQL DATABASE.
-
 # Connect  to databse with dbname postgres user postgres and password password1
-$dbc = pg_connect ( "host=10.10.7.190 dbname=postgres user=postgres password=password1" );
+
+
+$dbc = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=password1");
 
 ?>
